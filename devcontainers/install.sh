6201472e6ab1e32e3aef26a89eadefd969d9e5e6
Franklin Moy <franklinmoy3@gmail.com>
2024-07-13 16:03:56 -0700

    devcontainer install script now correctly sets working dir

--- devcontainers/install.sh
+++ devcontainers/install.sh
@@ -1,5 +1,30 @@
 #!/bin/bash
 
+# Silent pushd and popd
+pushd () {
+    command pushd "$@" > /dev/null
+}
+
+popd () {
+    command popd "$@" > /dev/null
+}
+
+prepare_install() {
+    # Preserve whatever dir the user was in before installing
+    pushd $(pwd)
+
+    # Force working dir to be the location of this script (linux/steamos)
+    SCRIPT_LOCATION="$(dirname "$0")"
+    cd $SCRIPT_LOCATION
+}
+
+# Get the repo root
+REPO_ROOT=$(git rev-parse --show-toplevel)
+echo dotfiles clone is rooted at \"$REPO_ROOT\"
+
+# Prepare by manipulating working directory
+prepare_install
+
 # Detect Linux distro
 distro=$(cat /etc/os-release | grep -e '^ID=' | cut -d '=' -f2)
 
@@ -11,4 +36,6 @@ case $distro in
     *)
         echo "Auto-config not yet available for \"$distro\""
     ;;
-esac    
+esac
+
+popd
